309071b2d9ba5c1b26b74c4fdc02c5476ed29b50
tej2707 <tejsharma2707@gmail.com>
2023-09-01 21:02:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+Open [http://tej2707.github.io/movie-search](http://tej2707.github.io/movie-search) to view it in your browser.
+
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React, { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import "./index.css";
+import App from "./components/App";
+import rootReducer from "./reducers";
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    if (typeof action !== "function") {
+      console.log("ACTION TYPE = ", action.type);
+    }
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+export const StoreContext = createContext();
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
